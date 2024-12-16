20997ebe4beccd9c610f0a2b48042ee29d01a545
Abdoul <abdoulkarim972012@gmail.com>
2024-12-16 13:04:12 +0000

    task frontend commit capstone

--- app/utils/axiosClient.js
+++ app/utils/axiosClient.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: 'https://taskmaster-backend-7hli.onrender.com/api/v1', // Remplacez par l'URL de votre backend Nest.js
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
+  withCredentials: true,
 });
 
 export default axiosClient;
